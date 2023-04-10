f17aae3a9769dbbe8b2fc3b9e35ecc91959126d4
lancierimaxi <lancierimaximilianoj@gmail.com>
2023-04-10 13:27:12 -0300

    Listo para la preentrega

--- js/main.js
+++ js/main.js
@@ -13,66 +13,89 @@ else{
 let saldo = prompt("Ingrese su saldo en pesos argentinos")
 console.log (saldo)
 
-let catalogo= [
+const catalogo= [
     {
+      id:1,
       marca: "Mouse Logitech",
       modelo: "Lightsync G203",
       precio: 8000
     },
     {
+      id:2,
       marca: "Teclado Soul",
       modelo: "XK800 QWERTY",
       precio: 4000
     },
     {
+      id:3,
       marca: "Auriculares Hyperx",
       modelo: "CLoud II",
       precio: 10000
     }
   ];
 
-  let catalogoServicios =[
+const catalogoServicios =[
     {
+        id:1,
         tipo: "Matenimiento",
         precio: 1500
     },
     {
+        id:2,
         tipo: "Actualizacion de Hardware",
         precio: 2000
     },
     {
+        id:3,
         tipo: "Instalacion de Sistema Operativo",
         precio: 1800
     }
   ]
 
 
-  function mostrarCatalogo() {
-    console.log("Catalogo Productos");
-    for (let i = 0; i < catalogo.length; i++) {
-      console.log(`${i + 1}. ${catalogo[i].marca} ${catalogo[i].modelo} - $${catalogo[i].precio}`);
-    }
+function mostrarCatalogo() {
+  console.log("Catalogo Productos");
+  for (let i = 0; i < catalogo.length; i++) {
+    console.log(`${i + 1}. ${catalogo[i].marca} ${catalogo[i].modelo} - $${catalogo[i].precio}`);
   }
+}
 
-  function mostrarCatalogoServicios(){
-    console.log("Catalogo Servicios");
-    for (let i = 0; i < catalogoServicios.length; i++) {
-        console.log(`${i + 1}. ${catalogoServicios[i].tipo} ${catalogoServicios[i].precio} `);
-    }
+function mostrarCatalogoServicios(){
+  console.log("Catalogo Servicios");
+  for (let i = 0; i < catalogoServicios.length; i++) {
+    console.log(`${i + 1}. ${catalogoServicios[i].tipo} ${catalogoServicios[i].precio} `);
   }
+}
 
-  let opcion = prompt("Selecciona que desea adquirir \n1-Productos \n2- Servicios");
+let opcion = prompt("Selecciona que desea adquirir \n1-Productos \n2- Servicios");
 switch (opcion) {
   case "1":
     alert("Seleccionaste Productos");
     mostrarCatalogo();
+    const catalogo = [
+      { id: 1, nombre: 'Mouse Logitech Lightsync G203', precio: 8000 },
+      { id: 2, nombre: 'Teclado Soul XK800 QWERTY', precio: 4000 },
+      { id: 3, nombre: 'Auricular Hyperx CLoud II', precio: 10000 },
+    ];
+    const catalogos = prompt("¿Qué producto desea? Recuerde ingresar el nombre tal cual ve en el catalogo")
+    const productoBuscado = catalogo.find( c => c.nombre == catalogos );
+        console.log(productoBuscado);
     break;
   case "2":
     alert("Seleccionaste Servicios");
     mostrarCatalogoServicios();
+    const catalogoServicios = [
+      { id: 1, nombre: 'Mantenimiento', precio: 1500 },
+      { id: 2, nombre: 'Actualizacion de Hardware', precio: 2000 },
+      { id: 3, nombre: 'Instalacion de Sistema Operativo', precio: 1800 },
+    ];
+    const catalogoServicioss = prompt("¿Qué servicio desea? Recuerde ingresar el nombre tal cual ve en el catalogo")
+    const servicioBuscado = catalogoServicios.find( sc => sc.nombre == catalogoServicioss );
+        console.log(servicioBuscado);
     break;
   
   default:
     alert("La opcion que intentas elegir es incorrecta");
     break;
 }
+
